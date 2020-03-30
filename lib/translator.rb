4cfb387126cda7545e3709fccd2b7636c010ea9f
Matteo Piccini <gnappo.1@live.it>
2020-03-30 04:01:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,20 @@
 # require modules here
 
 def load_library
-  # code goes here
-end
+  emoticons = YAML.load_file(file_path)
+
+    new_hash = {
+      'get_meaning' => {},
+      'get_emoticon' => {}
+    }
+
+    emoticons.each do |key,value|
+      binding.rpy
+      new_hash['get_emoticon'][value[0]] = emoticons[key][1]
+      new_hash['get_meaning'][value[1]] = key
+    end
+
+    new_hashend
 
 def get_japanese_emoticon
   # code goes here
